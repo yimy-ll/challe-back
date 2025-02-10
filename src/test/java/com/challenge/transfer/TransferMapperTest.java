555343994fc48e9c36663e0c429fc5011f5a4fe4
yimy llanos <llanosyimy@gmail.com>
2025-02-10 01:02:02 -0300

    add refactor infract ok

--- src/test/java/com/challenge/transfer/TransferMapperTest.java
+++ src/test/java/com/challenge/transfer/TransferMapperTest.java
@@ -5,12 +5,10 @@ import com.challenge.transfer.domain.Transfer;
 import com.challenge.transfer.domain.TransferType;
 import com.challenge.transfer.infrastructure.TransferMapper;
 import com.challenge.transfer.infrastructure.persistence.entity.TransferEntity;
-import com.challenge.transfer.infrastructure.web.TransferDto;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 public class TransferMapperTest {
-
     private final TransferMapper transferMapper = Mappers.getMapper(TransferMapper.class);
 
     @Test
@@ -43,37 +41,5 @@ public class TransferMapperTest {
         assertEquals(transferEntity.getId(), transfer.getId());
         assertEquals(transferEntity.getAmount(), transfer.getAmount());
         assertEquals(transferEntity.getTransferType(), transfer.getTransferType());
-    }
-
-    @Test
-    public void testTransferToTransferDto() {
-        Transfer transfer = Transfer.builder()
-                .id("test3")
-                .amount(2000.0)
-                .transferType(TransferType.CUENTA_CREDITO)
-                .build();
-
-        TransferDto transferDto = transferMapper.transferToTransferDTO(transfer);
-
-        assertNotNull(transferDto);
-        assertEquals(transfer.getId(), transferDto.getId());
-        assertEquals(transfer.getAmount(), transferDto.getAmount());
-        assertEquals(transfer.getTransferType(), transferDto.getTransferType());
-    }
-
-    @Test
-    public void testTransferDtoToTransfer() {
-        TransferDto transferDto = TransferDto.builder()
-                .id("test4")
-                .amount(2000.0)
-                .transferType(TransferType.CUENTA_CREDITO)
-                .build();
-
-        Transfer transfer = transferMapper.transferDTOToTransfer(transferDto);
-
-        assertNotNull(transferDto);
-        assertEquals(transfer.getId(), transferDto.getId());
-        assertEquals(transfer.getAmount(), transferDto.getAmount());
-        assertEquals(transfer.getTransferType(), transferDto.getTransferType());
     }
 }
