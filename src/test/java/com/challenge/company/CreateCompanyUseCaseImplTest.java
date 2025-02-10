1f11b46424ae50d84ab180432bf5f43b74c158c7
yimy llanos <llanosyimy@gmail.com>
2025-02-10 03:10:46 -0300

    add test company ok

---
+++ src/test/java/com/challenge/company/CreateCompanyUseCaseImplTest.java
@@ -0,0 +1,65 @@
+package com.challenge.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.challenge.company.application.useCases.CreateCompanyUseCaseImpl;
+import com.challenge.company.domain.Company;
+import com.challenge.company.domain.exception.CompanyAlreadyExistsException;
+import com.challenge.company.domain.ports.out.CompanyRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+@ExtendWith(MockitoExtension.class)
+class CreateCompanyUseCaseImplTest {
+    private final LocalDateTime DATA_TIME_TEST = LocalDateTime.of(2021, 1, 1, 0, 0);
+
+    @Mock
+    private CompanyRepository companyRepository;
+
+    @InjectMocks
+    private CreateCompanyUseCaseImpl createCompanyUseCase;
+
+    @Test
+    @DisplayName("Debe crear una empresa correctamente")
+    void createCompany_Success() {
+        Company company = Company.builder()
+                .dateOfAccession(DATA_TIME_TEST)
+                .name("Tech Solutions")
+                .build();
+
+        when(companyRepository.existsCompanyByName(company.getName())).thenReturn(false);
+        when(companyRepository.save(any(Company.class))).thenReturn(company);
+
+        Company result = createCompanyUseCase.createCompany(company);
+
+        assertNotNull(result);
+        verify(companyRepository, times(1)).existsCompanyByName(company.getName());
+        verify(companyRepository, times(1)).save(company);
+    }
+
+    @Test
+    @DisplayName("Debe lanzar una excepción si la empresa ya existe")
+    void createCompanyThrowsExceptionWhenDuplicate() {
+        Company company = Company.builder()
+                .name("Tech Solutions")
+                .dateOfAccession(DATA_TIME_TEST)
+                .build();
+        when(companyRepository.existsCompanyByName(company.getName())).thenReturn(true);
+
+        CompanyAlreadyExistsException exception = assertThrows(CompanyAlreadyExistsException.class, () -> {
+            createCompanyUseCase.createCompany(company);
+        });
+
+        assertEquals(CompanyAlreadyExistsException.class.getSimpleName(), exception.getClass().getSimpleName());
+        verify(companyRepository, times(1)).existsCompanyByName(company.getName());
+        verify(companyRepository, never()).save(any(Company.class));
+    }
+}
+
