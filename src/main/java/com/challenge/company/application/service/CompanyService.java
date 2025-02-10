8a4c65ea3bb7df8528af11e1fe898ded9ad1e38e
yimy llanos <llanosyimy@gmail.com>
2025-02-10 09:42:38 -0300

    capa  de campany finish ok

--- src/main/java/com/challenge/company/application/service/CompanyService.java
+++ src/main/java/com/challenge/company/application/service/CompanyService.java
@@ -5,7 +5,6 @@ import com.challenge.company.domain.ports.in.CreateCompanyUseCase;
 import com.challenge.company.domain.ports.in.GetCompaniesByFilterUseCase;
 import com.challenge.company.infrastructure.web.filter.FilterCompany;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
